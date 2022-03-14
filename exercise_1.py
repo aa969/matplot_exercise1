8d268e597ab803c4e7e1769d409a65f95b379508
aa969 <adrees.arbab@gmail.com>
2022-03-14 12:35:47 +0000

    test

---
+++ exercise_1.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+
+ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+
+dev_y = [38496, 42000, 46752, 49320, 53200,
+         56000, 62316, 64928, 67317, 68748, 73752]
+
+Y2 = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
+
+plt.plot(ages_x, dev_y, label="All dev")
+plt.plot(ages_x, Y2, label="Python")
+
+plt.xlabel('Age')
+plt.ylabel('Median Salary (USD)')
+plt.title("Median salary (USD) by Age")
+plt.legend()
+
+
+plt.show()
